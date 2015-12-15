1baae4572986f61e15d8572eba96ee7fab021553
Madarow <mathieu.lauffenburger.dev@gmail.com>
2015-12-15 14:49:26 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
+<p>HELLO</p>
         <style>
             html, body {
                 height: 100%;
