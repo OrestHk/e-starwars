2dc414cf10b08d0bbd307beba88d230bc5428c28
OrestHk <hrynchuk.orest@gmail.com>
2015-12-15 11:22:28 +0100

    Modules (php, node) installed, service providers plugged, composer.json and package.json updated, gulpfile reset

--- config/app.php
+++ config/app.php
@@ -145,6 +145,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * My Service Providers
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -193,6 +199,11 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        // Custom Aliases
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Debugbar'  => Barryvdh\Debugbar\Facade::class,
 
     ],
 
