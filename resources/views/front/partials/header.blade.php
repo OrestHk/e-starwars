cc862e65ec5bbd8e98be128560eac57163ca08e6
OrestHk <hrynchuk.orest@gmail.com>
2016-01-03 22:48:58 +0100

    Change structure ressources/assets/, create gulpfile

--- resources/views/front/partials/header.blade.php
+++ resources/views/front/partials/header.blade.php
@@ -3,9 +3,9 @@
         @include('front.partials.menu')
         <a href="{{url('/')}}" title="Kuar's resell"><h1>
             <span class="name name-1">Kuar's</span>
-            <img src="" alt="Logo" />
+            <img src="{{asset('assets/images/logo.png')}}" alt="Logo" />
             <span class="name name-2">Resell</span>
-        <h1></a>
+        </a></h1>
         @include('front.partials.cart')
     </div>
 </header>
