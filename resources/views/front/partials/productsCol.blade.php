1280e5e351225bac2cf63c3ef6b7dc41c3cd057b
OrestHk <hrynchuk.orest@gmail.com>
2016-01-04 21:32:32 +0100

    No products available handeled

--- resources/views/front/partials/productsCol.blade.php
+++ resources/views/front/partials/productsCol.blade.php
@@ -1,19 +1,23 @@
 <div class="products">
-    <div class="left blocs">
-        @forelse($prods['left'] as $product)
-            @include('front.partials.product', array($product, $single = false))
-        @empty
-            <p>No products available yet.</p>
-        @endforelse
-        <div class="clear"></div>
-    </div>
-    <div class="right blocs">
-        @forelse($prods['right'] as $product)
-            @include('front.partials.product', array($product, $single = false))
-        @empty
-            <p>No products available yet.</p>
-        @endforelse
-        <div class="clear"></div>
-    </div>
+    @if($prods['left'])
+        <div class="left blocs">
+            @foreach($prods['left'] as $product)
+                @include('front.partials.product', array($product, $single = false))
+            @endforeach
+            <div class="clear"></div>
+        </div>
+    @endif
+    @if($prods['right'])
+        <div class="right blocs">
+            @foreach($prods['right'] as $product)
+                @include('front.partials.product', array($product, $single = false))
+            @endforeach
+            <div class="clear"></div>
+        </div>
+    @endif
+
+    @if(!$prods['left'] && !$prods['right'])
+        <p>No products available yet.</p>
+    @endif
     <div class="clear"></div>
 </div>
