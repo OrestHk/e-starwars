7a94704c60e244c72842a0d8b5ac004905afbcf2
Madarow <mathieu.lauffenburger.dev@gmail.com>
2016-01-05 14:08:02 +0100

    cart in cookie

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -106,7 +106,17 @@ class FrontController extends Controller
     }
 
     public function order(){
-      return view('front.order.index');
+        $orders = json_decode($_COOKIE['SwC']);
+        $products = [];
+        $cost = 0;
+        foreach ($orders as $order => $qtn) {
+            $product = Product::find($order)->with('tags','category','picture')->firstOrFail();
+            array_push($products,$product);
+            $product->final_price = $product->price * $qtn;
+            $cost += $product->final_price;
+        }
+        setcookie('SwCcostFinal',$cost);
+        return view('front.order.index',compact('products','cost'));
     }
 
     public function getOrderProduct(Request $request){
@@ -120,22 +130,18 @@ class FrontController extends Controller
     public function validationOrder(Request $request){
 
         $rq = $request->all();
-
+        $orders = json_decode($_COOKIE['SwC']);
         $user = User::where('email',$rq['email'])->firstOrFail();
         if(!empty($user)){
-
-
-           $history = History::create([
+            $history = History::create([
                 'user_id'=>$user->id,
                 'order_date'=>date('Y-m-d H:m:s',time()),
-                'total_price'=>$rq['total']
+                'total_price'=>$_COOKIE['SwCcostFinal']
             ]);
-
-
-            foreach($rq['order'] as $product_id => $quantity){
+            foreach($orders as $product_id => $quantity){
                 $history->products()->attach([$history->id=>['quantity'=>$quantity,'product_id'=>$product_id]]);
             }
-
+            setcookie('SwCcostFinal','off',time()-1);
             return 1;
         }else{
             return 0;
--- public/assets/js/cartJs.js
+++ public/assets/js/cartJs.js
@@ -1,141 +1,137 @@
-  'use strict'
-  var Cart = {
+'use strict'
+var Cart = {
     totalPrice:0,
     product:[],
     order:{},
 
     addOderItem:function(id,quantity){
-      this.order[id] = quantity;
-      this.orderToString();
+        this.order[id] = quantity;
+        this.orderToCookie();
     },
 
-    orderToString:function(){
-        var string = localStorage.getItem('command');
-
-        var order;
-        for(order in this.order){
-          if(typeof(string)===typeof(this))
-            string = order+'_'+this.order[order];
-          else
-            string += '|'+order+'_'+this.order[order];
-        }
-        localStorage.setItem('command', string);
+    orderToCookie: function () {
+        Cookies.set('SwC',this.order);
+        console.log(Cookies.get());
     },
+
+
 
     orderToArray:function(){
-      var arrayTest = [];
-      var string = localStorage.command;
-      if(typeof(string) == typeof('string')){
-        if(typeof(string.split('|')) === typeof(arrayTest)){
-          string = localStorage.command.split('|');
-          for(var i = 0 ; i < string.length ; i++){
-            var str = string[i].split('_');
-            this.order[str[0]]=str[1];
-          }
-        }else{
-          var str = localStorage.command.split('_');
-          this.order[str[0]]=str[1];
+        var arrayTest = [];
+        var string = localStorage.command;
+        if(typeof(string) == typeof('string')){
+            if(typeof(string.split('|')) === typeof(arrayTest)){
+                string = localStorage.command.split('|');
+                for(var i = 0 ; i < string.length ; i++){
+                    var str = string[i].split('_');
+                    this.order[str[0]]=str[1];
+                }
+            }else{
+                var str = localStorage.command.split('_');
+                this.order[str[0]]=str[1];
+            }
         }
-      }
     },
-
-    orderList:function(){
-
-        this.orderToArray();
-        var order;
-        var tab = [];
-        for(order in this.order){
-            tab.push(order);
-        }
-        this.AjaxProduct(tab);
-
-    },
-
-    AjaxProduct:function(ids){
+    orderProced: function () {
         var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-      $.ajax({
-          url:'/orderObj/',
-          type: 'POST',
-          data: {
-              _token: CSRF_TOKEN,
-              'ids':ids
-              },
-          dataType: 'JSON',
-          complete: function (data){
-              Cart.productToHTML(JSON.parse(data.responseText));
-          },
-          error:function(error){
-            console.log(error);
-            //alert('Whoops !! error:'+error);
-          }
-      });
-    },
+        var name = $('#CustomerName').val();
+        var mail = $('#CustomerMail').val();
+        console.log(name,mail);
+        $.ajax({
+            url:'/validationOrder/',
+            type: 'POST',
+            data: {
+                _token: CSRF_TOKEN,
+                order:this.order,
+                name:name,
+                email:mail,
+                total:this.totalPrice
+            },
+            dataType: 'JSON',
+            complete: function (data){
+                localStorage.clear();
+            },
+            error:function(error){
+                console.log(error);
+                //alert('Whoops !! error:'+error);
+            }
+        });
+    }
+    /*
+     orderList:function(){
 
-    productToHTML:function(product){
-        console.log(product);
-        var total = 0;
-        for(var i = 0; i < product.length ;i++){
-            this.totalPrice += this.order[product.id] * product[i].price;
-            var _html = '<div class=""><p>name: '+product[i].name+'</p>'+
-                '<img src="assets/images/products/'+product[i].picture.filename+'">'+
-                '<p>price: '+product[i].price+'</p>'+
-                '<p>quantity: '+this.order[product[i].id]+'</p>'+
-                '<p>final cost: '+this.order[product[i].id] * product[i].price+'</p></div>';
-            total += this.order[product[i].id] * product[i].price;
-            $('#orderList').append(_html);
-        }
-        this.totalPrice = total;
-        $('#orderList').append('<p>total = '+total+'</p>');
+     this.orderToArray();
+     var order;
+     var tab = [];
+     for(order in this.order){
+     tab.push(order);
+     }
+     this.AjaxProduct(tab);
 
-    },
-      orderProced: function () {
-          var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-          var name = $('#CustomerName').val();
-          var mail = $('#CustomerMail').val();
-          console.log(name,mail);
-          $.ajax({
-              url:'/validationOrder/',
-              type: 'POST',
-              data: {
-                  _token: CSRF_TOKEN,
-                  order:this.order,
-                  name:name,
-                  email:mail,
-                  total:this.totalPrice
-              },
-              dataType: 'JSON',
-              complete: function (data){
-                  localStorage.clear();
-              },
-              error:function(error){
-                  console.log(error);
-                  //alert('Whoops !! error:'+error);
-              }
-          });
-      }
+     },
 
-  }
+     AjaxProduct:function(ids){
+     var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+     $.ajax({
+     url:'/orderObj/',
+     type: 'POST',
+     data: {
+     _token: CSRF_TOKEN,
+     'ids':ids
+     },
+     dataType: 'JSON',
+     complete: function (data){
+     Cart.productToHTML(JSON.parse(data.responseText));
+     },
+     error:function(error){
+     console.log(error);
+     //alert('Whoops !! error:'+error);
+     }
+     });
+     },
+
+     productToHTML:function(product){
+     console.log(product);
+     var total = 0;
+     for(var i = 0; i < product.length ;i++){
+     this.totalPrice += this.order[product.id] * product[i].price;
+     var _html = '<div class=""><p>name: '+product[i].name+'</p>'+
+     '<img src="assets/images/products/'+product[i].picture.filename+'">'+
+     '<p>price: '+product[i].price+'</p>'+
+     '<p>quantity: '+this.order[product[i].id]+'</p>'+
+     '<p>final cost: '+this.order[product[i].id] * product[i].price+'</p></div>';
+     total += this.order[product[i].id] * product[i].price;
+     $('#orderList').append(_html);
+     }
+     this.totalPrice = total;
+     $('#orderList').append('<p>total = '+total+'</p>');
+
+     },*/
+
+
+}
 
 
 
-  $(document).ready(function(){
+$(document).ready(function(){
 
     var isProduct = location.pathname.split('/')[1] == 'products' ? true : false;
     var isOrder = location.pathname.split('/')[1] == 'order' ? true : false;
     var checkekCart = localStorage.getItem('command');
-      if(typeof(checkekCart) !== typeof(Cart) && isProduct){
-          Cart.orderToArray();
-      }
-      if(isOrder){
-        Cart.orderList();
-      }
-  });
+    if(typeof(checkekCart) !== typeof(Cart) && isProduct){
+        Cart.orderToArray();
+    }
+    if(isOrder){
+        //Cart.orderList();
+    }
+});
 
-  $('#orderValidation').submit(function(evt){
-      evt.preventDefault();
-     Cart.orderProced();
-  });
+$('#orderValidation').submit(function(evt){
+    evt.preventDefault();
+    Cart.orderProced();
+});
 
-  $('input[name="order"]').on('click',function(){
-      Cart.addOderItem($(this).attr('data-id'),$('#quantity :selected').text());
-  });
+$('input[name="order"]').on('click',function(evt){
+    evt.preventDefault();
+    Cart.addOderItem($('input[name="product_id"]').val(),$('#quantity :selected').text());
+});
