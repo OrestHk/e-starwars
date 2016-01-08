ae677ee3a36b2e0f71d180cb7d131886636b7426
Madarow <mathieu.lauffenburger.dev@gmail.com>
2016-01-09 00:27:11 +0100

    add security B.O nav

--- resources/views/admin/partials/admin_menu.blade.php
+++ resources/views/admin/partials/admin_menu.blade.php
@@ -1,7 +1,9 @@
 
 <nav class="row" id="adminMainNav">
     <a class="three columns" href="{{url('/')}}">site public</a>
-    <a class="three columns" href="{{url('auth/logout')}}">logout</a>
-    <a class="three columns" href="{{url('admin/product')}}">administrer les produits</a>
-    <a class="three columns" href="{{url('admin/history')}}">voir les commandes</a>
+    @if(Auth::check())
+        <a class="three columns" href="{{url('auth/logout')}}">logout</a>
+        <a class="three columns" href="{{url('admin/product')}}">administrer les produits</a>
+        <a class="three columns" href="{{url('admin/history')}}">voir les commandes</a>
+    @endif
 </nav>
